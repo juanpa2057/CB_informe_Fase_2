58ad1694bd122562aa6eef94909227827be6e627
jmcajigas <115262308+jmcajigas@users.noreply.github.com>
2025-07-22 15:43:51 -0500

    Informe semanal del 12 al 20 de julio 2025 F2
    Informe semanal del 12 al 20 de julio 2025 F2

--- config_v2.py
+++ config_v2.py
@@ -4,9 +4,9 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically all
 # dates must be mondays.
-BASELINE = ['2025-02-01', '2025-06-30']
-STUDY = ['2025-06-30', '2025-07-07']
-PAST_WEEK = ['2025-06-23', '2025-06-30']
+BASELINE = ['2025-03-01', '2025-07-14']
+STUDY = ['2025-07-14', '2025-07-21']
+PAST_WEEK = ['2025-07-07', '2025-07-14']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
