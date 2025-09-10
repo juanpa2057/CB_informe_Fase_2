5c37b34a5e6f66dcff755e509b8ede4d53c3e334
Juan Manuel Cajigas Eraso <115262308+jmcajigas@users.noreply.github.com>
2025-09-10 14:27:01 -0500

    Informe semanal del 1 al 7 de septiembre
    Informe semanal del 1 al 7 de septiembre

--- config_v2.py
+++ config_v2.py
@@ -4,9 +4,9 @@ NIGHT_HOURS = [0, 1, 2, 3, 4, 5, 19, 20, 21, 22, 23]
 # last baseline date must be the same
 # date as start of study. Basically all
 # dates must be mondays.
-BASELINE = ['2025-05-01', '2025-08-25']
-STUDY = ['2025-08-25', '2025-09-01']
-PAST_WEEK = ['2025-08-18', '2025-08-25']
+BASELINE = ['2025-06-01', '2025-09-01']
+STUDY = ['2025-09-01', '2025-09-08']
+PAST_WEEK = ['2025-08-25', '2025-09-01']
 
 DATE_INTERVALS_TO_DISCARD = {
 }
